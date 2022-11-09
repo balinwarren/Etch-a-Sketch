f4471bd0329fb9d290c8afbb1247472318f58082
Balin Warren <balin.warren@gmail.com>
2022-11-08 16:48:38 -0800

    Added event listener for color picker

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let sliderListener = document.getElementById("size-slider").addEventListener("input", sizeChange);
+let colorListener = document.getElementById("color-picker").addEventListener("change", colorChange);
+
+let currentColor = "";
 
 function sizeChange() {
   let size = document.getElementById("size-slider").value;
 
   document.getElementById("size-value").textContent = size + " x " + size;
+}
+
+function colorChange() {
+  let currentColor = document.getElementById("color-picker").value;
+
+  console.log(currentColor);
 }
