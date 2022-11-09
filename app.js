6b82877bd23d0adf7957b7fe3289cb77d7593ff6
Balin Warren <balin.warren@gmail.com>
2022-11-09 12:49:30 -0800

    Added generate grid function

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const canvas = document.getElementById("canvas");
+
 let sliderListener = document.getElementById("size-slider").addEventListener("input", sizeChange);
+let gridChange = document.getElementById("size-slider").addEventListener("change", generateGrid);
 let colorListener = document.getElementById("color-picker").addEventListener("change", colorChange);
 
-let currentColor = "";
+let currentColor = document.getElementById("color-picker").value;
+
+generateGrid();
 
 function sizeChange() {
   let size = document.getElementById("size-slider").value;
@@ -10,7 +15,23 @@ function sizeChange() {
 }
 
 function colorChange() {
-  let currentColor = document.getElementById("color-picker").value;
+  currentColor = document.getElementById("color-picker").value;
 
   console.log(currentColor);
+}
+
+function generateGrid() {
+  let size = document.getElementById("size-slider").value;
+
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.lastChild);
+  }
+  
+  canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement('div');
+    canvas.appendChild(gridElement);
+  }
 }
